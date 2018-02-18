84a4121c3c630e46dc3b1a58251026f59d3a3879
ugurcanpolat <polatu@itu.edu.tr>
2018-02-18 16:17:48 +0300

    Implement sundays1Iter, tail recursive function of sundays1

--- sundays.hs
+++ sundays.hs
@@ -33,3 +33,21 @@ sundays1 start end = sundays' start 1
           | m >= 12   = 1
           | otherwise = m + 1
         rest = sundays' nextY nextM
+
+sundays1Iter :: Integer -> Integer -> Integer
+sundays1Iter start end = sundays' start 1 0
+  where
+    sundays' :: Integer -> Integer -> Integer -> Integer
+    sundays' y m acc
+      | y > end   = acc
+      | otherwise = iter
+      where
+        nextY 
+          | m >= 12   = y + 1
+          | otherwise = y
+        nextM 
+          | m >= 12   = 1
+          | otherwise = m + 1
+        iter
+          | dayOfWeek y m 1 == 1 = sundays' nextY nextM (acc + 1)
+          | otherwise            = sundays' nextY nextM acc
