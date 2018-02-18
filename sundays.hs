d00919b2f2fb2254214b67831308d0351183f69a
ugurcanpolat <polatu@itu.edu.tr>
2018-02-18 16:25:05 +0300

    sundays2: Implement daysInMonth function

--- sundays.hs
+++ sundays.hs
@@ -54,3 +54,9 @@ sundays1Iter start end = sundays' start 1 0
 
 leap :: Integer -> Bool
 leap y = (y `mod` 4 == 0) && (y `mod` 100 /= 0) || (y `mod` 400 == 0)
+
+daysInMonth :: Integer -> Integer -> Integer
+daysInMonth y m 
+  | m == 2 = if leap y then 29 else 28
+  | (m == 4) || (m == 6) || (m == 9) || (m == 11) = 30
+  | otherwise = 31
