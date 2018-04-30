e7cea5f715cb85e5825e25e241950b92590ce307
ugurcanpolat <polatu@itu.edu.tr>
2018-04-30 18:28:40 +0300

    Bonus3: Implement wordCharCounts function

--- Bonus3/Main.hs
+++ Bonus3/Main.hs
@@ -4,10 +4,18 @@
    Date: 04.05.2018
 -}
 
+import Prelude hiding (Word)
 import Data.Char 
-import Data.Map
+import Data.Map hiding (map)
 import Data.List
 import System.Environment
+
+type Word      = [Char]
+type Sentence  = [Word]
+type CharCount = [(Char,Int)]
+
+wordCharCounts :: Word -> CharCount
+wordCharCounts = map (\c -> (head c, length c)) . group . map toLower
 
 main = do args <- getArgs
           let sentence = head args
