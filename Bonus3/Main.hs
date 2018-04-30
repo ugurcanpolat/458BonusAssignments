1062bcb8bdb2edbc191b5213f46475a7c0a027e3
ugurcanpolat <polatu@itu.edu.tr>
2018-04-30 22:16:01 +0300

    Bonus3: Implement wordAnagrams function

--- Bonus3/Main.hs
+++ Bonus3/Main.hs
@@ -25,6 +25,9 @@ dictCharCounts = map (\w -> (w, wordCharCounts w))
 dictWordsByCharCounts :: [(Word,CharCount)] -> [(CharCount,[Word])]
 dictWordsByCharCounts = toList . fromListWith (++) . map (\(w,cc) -> (cc, [w]))
 
+wordAnagrams :: Word -> [(CharCount,[Word])] -> [Word]
+wordAnagrams w ccs = head [ws | (cc,ws) <- ccs, cc == wordCharCounts w]
+
 main = do args <- getArgs
           let sentence = head args
           putStrLn sentence
