d168ed35a8a592fde5278187783ca3d0cf934610
ugurcanpolat <polatu@itu.edu.tr>
2018-05-03 14:07:57 +0300

    Bonus3: Add comments to explain not explicit parts

--- Bonus3/Main.hs
+++ Bonus3/Main.hs
@@ -28,10 +28,10 @@ dictWordsByCharCounts = toList . fromListWith (++) . map (\(w,cc) -> (cc, [w]))
 wordAnagrams :: Word -> [(CharCount,[Word])] -> [Word]
 wordAnagrams w ccs = case result of 
     [] -> []
-    _  -> head result
+    _  -> head result -- Extract one element of list
   where
-    comp = sortBy (\(a,_) (b,_) -> compare a b) $ wordCharCounts w
-    result = [ws | (cc,ws) <- ccs, sortBy (\(a,_) (b,_) -> compare a b) cc == comp]
+    comp = sortBy (\(a,_) (b,_) -> compare a b) $ wordCharCounts w -- Compare
+    result = [ws | (cc,ws) <- ccs, sortBy (\(a,_) (b,_) -> compare a b) cc == comp] -- Find anagrams
 
 charCountsSubsets :: CharCount -> [CharCount]
 charCountsSubsets = map wordCharCounts . wordSubset . createWord
@@ -40,7 +40,8 @@ charCountsSubsets = map wordCharCounts . wordSubset . createWord
     wordSubset [] = [[]]
     wordSubset (c:cs) = nub ([c:cs' | cs' <- wordSubset cs] ++ wordSubset cs)
 
-createWord :: CharCount -> Word
+-- Create word from char counts
+createWord :: CharCount -> Word 
 createWord cc = foldr (\a b -> a ++ b) "" [replicate n c | (c,n) <- cc]
 
 subtractCounts :: CharCount -> CharCount -> CharCount
@@ -49,38 +50,38 @@ subtractCounts a b = [(c,abs n) | (c,n) <- toList (fromListWith (-) (a ++ b)), n
 sentenceAnagrams :: [Word] -> [(CharCount,[Word])] -> [[Word]]
 sentenceAnagrams sent dict = helper sentsubs sentcc [] [[]]
   where
-    sentcc   = toList $ fromListWith (+) $ foldr (\a b -> a ++ b) [] [a | a <- sentenceCharCounts sent]
-    sentsubs = charCountsSubsets sentcc
-    lengBase = lengthCharCount sentcc
+    sentcc   = toList $ fromListWith (+) $ foldr (\a b -> a ++ b) [] [a | a <- sentenceCharCounts sent] -- Char count
+    sentsubs = charCountsSubsets sentcc -- Base subsets of char counts
+    lengBase = lengthCharCount sentcc -- Goal anagram length
 
     helper :: [CharCount] -> CharCount -> [[Word]] -> [[Word]] -> [[Word]]
     helper subsets cc anagrams acc = case subsets of
-        []        -> anagrams
-        (ss:sses) -> if ss == [] then helper sses cc anagrams acc
-                                 else helper sses cc anagrams' acc
+        []        -> anagrams -- All subsets are iterated
+        (ss:sses) -> if ss == [] then helper sses cc anagrams acc -- Empty subset
+                                 else helper sses cc anagrams' acc -- Iterate over subsets
         where
-          s   = if subsets == [] then [] else head subsets
-          ses = if subsets == [] then [] else tail subsets
+          s   = if subsets == [] then [] else head subsets -- Check if subsets empty
+          ses = if subsets == [] then [] else tail subsets -- Check if subsets empty
           
-          an = (wordAnagrams (createWord s) dict) 
+          an = (wordAnagrams (createWord s) dict) -- Anagram of a subset
 
-          acc' = case an of 
+          acc' = case an of -- Accumulator of anagram of words
             [] -> [[]]
             _  -> [x ++ [y] | x <- acc, y <- an]
           
-          subsets' = case an of 
+          subsets' = case an of -- Current subsets
             []    -> ses
             (a:_) -> charCountsSubsets cc'
 
-          cc' = case an of           
+          cc' = case an of -- Remaining character counts        
             []    -> cc
             (a:_) -> subtractCounts cc $ wordCharCounts a
 
-          anagrams' = case acc' of 
+          anagrams' = case acc' of -- New anagram case
             [[]]  -> anagrams
-            (a:_) -> if lengthWords a == lengBase && an /= []
+            (a:_) -> if lengthWords a == lengBase && an /= [] -- Anagram of sentece found
                        then anagrams ++ acc'
-                       else helper subsets' cc' anagrams acc'
+                       else helper subsets' cc' anagrams acc' -- Anagram word found iterate to find sentence
 
 lengthCharCount :: CharCount -> Int
 lengthCharCount cc = foldr (\a b -> a + b) 0 [n | (_,n) <- cc]
@@ -102,9 +103,10 @@ printAnagrams :: [[Word]] -> IO()
 printAnagrams s = putStrLn $ init $ unlines str
   where
     str = case s of 
-      [] -> ["*** No anagram found in the dictionary ***"]
+      [] -> ["*** No anagram found in the dictionary ***"] -- Empty anagram case
       _  -> printSentence s []
 
+    -- Combine all anagram words in list of string 
     printSentence :: [[Word]] -> [String] -> [String]
     printSentence s acc = case s of 
       []       -> acc
