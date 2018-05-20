446fa4b3068191db9778b8755aabf2c836490099
ugurcanpolat <polatu@itu.edu.tr>
2018-05-20 22:22:09 +0300

    TermProject: Implement insert function

--- TermProject/trie.hs
+++ TermProject/trie.hs
@@ -1,17 +1,26 @@
+{- @Author
+   Student Name: Ugurcan Polat
+   Student ID : 150140117
+   Date: 21.05.2018
+-}
+
 import qualified Data.Map as M
 import Data.Maybe
 import System.Environment
 import System.IO
 import Prelude hiding (Word)
 
-data Trie = Trie {end :: Bool, children :: M.Map Char Trie}
+data Trie = Trie {end :: Bool, children :: M.Map Char Trie} deriving (Show)
 type Word = String
 
 empty :: Trie
-empty = Trie {end = True, children = M.empty}
+empty = Trie {end = False, children = M.empty}
 
 insert :: Word -> Trie -> Trie
-insert = undefined
+insert [] (Trie _ m)     = Trie True m
+insert (c:cs) (Trie e m) = case M.lookup c m of
+    Nothing -> Trie e $ M.insert c (insert cs empty) m
+    Just t  -> Trie e $ M.insert c (insert cs t) m
 
 insertList :: [Word] -> Trie
 insertList = undefined
