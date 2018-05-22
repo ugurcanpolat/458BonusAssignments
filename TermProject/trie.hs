a7297b57a7a0251a6cef4341754913c3bacd9c1a
ugurcanpolat <polatu@itu.edu.tr>
2018-05-22 21:37:09 +0300

    TermProject: Add necessary comments to explain algorithms

--- TermProject/trie.hs
+++ TermProject/trie.hs
@@ -17,19 +17,19 @@ empty :: Trie
 empty = Trie {end = False, children = M.empty}
 
 insert :: Word -> Trie -> Trie
-insert [] (Trie _ m)     = Trie True m
-insert (c:cs) (Trie e m) = case M.lookup c m of
-    Nothing -> Trie e $ M.insert c (insert cs empty) m
-    Just t  -> Trie e $ M.insert c (insert cs t) m
+insert [] (Trie _ m)     = Trie True m -- end of trie
+insert (c:cs) (Trie e m) = case M.lookup c m of -- if character is already in trie
+    Nothing -> Trie e $ M.insert c (insert cs empty) m -- not
+    Just t  -> Trie e $ M.insert c (insert cs t) m -- already in trie
 
 insertList :: [Word] -> Trie
 insertList = foldr insert empty 
 
 search :: Word -> Trie -> Bool
-search [] (Trie e _)     = e
+search [] (Trie e _)     = e -- found?
 search (c:cs) (Trie _ m) = case M.lookup c m of 
-    Nothing -> False
-    Just t  -> search cs t
+    Nothing -> False -- not found
+    Just t  -> search cs t -- character found, continue with other characters
 
 getWords :: Trie -> [Word]
 getWords (Trie _ m) = traverser (M.toList m) [] []
@@ -38,25 +38,25 @@ getWords (Trie _ m) = traverser (M.toList m) [] []
     traverser [] _ acc = acc
     traverser l wAcc acc
       | e == True = if M.null m' == True
-                       then traverser ts ws (acc ++ [w])
-                       else traverser newList ((replicate size w) ++ ws) (acc ++ [w])
+                       then traverser ts ws (acc ++ [w]) -- end of sub-trie
+                       else traverser newList ((replicate size w) ++ ws) (acc ++ [w]) -- end of word but sub-trie has characters
       | otherwise = traverser newList ((replicate size w) ++ ws) acc
         where
-            t  = head l
-            ts = if null l then [] else tail l 
-            c  = fst t
-            tr = snd t
+            t  = head l -- head of list of pairs
+            ts = if null l then [] else tail l  -- tail of list of pairs
+            c  = fst t -- character of pair
+            tr = snd t -- trie of pair
 
-            getMap (Trie _ m) = m
-            getEnd (Trie e _) = e
+            getMap (Trie _ m) = m -- map part of trie
+            getEnd (Trie e _) = e -- bool part of trie
 
             e  = getEnd tr
             m' = getMap tr
 
-            size = M.size m'
+            size = M.size m' -- size of map
             newList = M.toList m' ++ ts
-            w   = if null wAcc then [c] else head wAcc ++ [c]
-            ws  = if null wAcc then [] else tail wAcc
+            w   = if null wAcc then [c] else head wAcc ++ [c] -- insert to word accumulator
+            ws  = if null wAcc then [] else tail wAcc -- tail of word  accumulator
 
 prefix :: Word -> Trie -> Maybe [Word]
 prefix w t = if null found then Nothing else Just found
@@ -82,21 +82,21 @@ getInput = do putStrLn "a) Add Word"
               putStrLn "p) Print all words"
               putStrLn "e) Exit"
               putStrLn "Enter the action:"
-              a <- getLine
+              a <- getLine -- get action character
               let c = if null a then 'e' else head a -- 'e' means exit
               if c `elem` "aA" || c `elem` "sS" || c `elem` "fF" 
-                then do putStrLn "Enter word/prefix:"
-                        wp <- getLine
+                then do putStrLn "Enter word/prefix:" 
+                        wp <- getLine -- get word/prefix
                         return (convertAction c wp)
                 else return (convertAction c [])
 
 doAction :: Action -> Trie -> IO Trie
-doAction a t = case a of 
+doAction a t = case a of
     Add w    -> do putStrLn "New word is added!\n" 
-                   return $ insert w t
+                   return $ insert w t -- return modified trie
     Search w -> if search w t 
                   then do putStrLn "Exists in dictionary!\n" 
-                          return t
+                          return t -- return not-modified trie
                   else do putStrLn "NOT exist!\n"
                           return t
     Find w   -> case prefix w t of 
@@ -110,15 +110,15 @@ doAction a t = case a of
                    return t
     Exit     -> return empty  
   where
-    printWords :: [Word] -> IO ()
+    printWords :: [Word] -> IO () -- prints words line by line
     printWords = putStrLn . unlines
 
 runProgram :: Trie -> IO ()
-runProgram t = if t == empty 
+runProgram t = if t == empty -- if trie is empty, no need to take action
                  then return ()
                  else do action <- getInput
                          t' <- doAction action t
-                         runProgram t'
+                         runProgram t' -- run again
 
 main = do args <- getArgs
           let fileName = head args
